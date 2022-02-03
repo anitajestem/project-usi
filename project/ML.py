ae6c461a05a96f9a9c9f496b2f6feeac34cc44db
anitajestem <176796@student.ue.wroc.pl>
2022-02-03 21:35:40 +0100

    update_03 02

--- project/ML.py
+++ project/ML.py
@@ -12,14 +12,14 @@ Y = np.array([30, 55, 75, 95, 115, 30, 65, 75, 105, 115, 45, 85, 150, 200, 250, 
 data = pd.DataFrame({'Ads/Month':X, 'Paid/Month':Y})
 print(data.head())
 
-# sns.regplot(x='Ads/Month', y='Paid/Month', data=data)
-# plt.title("Regression Plot Monthly")
-# plt.show()
-#
-# model = LinearRegression().fit(X.reshape(-1, 1), Y)
-#
+sns.regplot(x='Ads/Month', y='Paid/Month', data=data)
+plt.title("Regression Plot Monthly")
+plt.show()
+
+model = LinearRegression().fit(X.reshape(-1, 1), Y)
+
 filename = "model.sav"
-# joblib.dump(model, filename)
+joblib.dump(model, filename)
 
 loaded_model = joblib.load(filename)
 print(loaded_model.predict([[20]]))
